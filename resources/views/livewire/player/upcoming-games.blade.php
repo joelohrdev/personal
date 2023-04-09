76832ce0f59d774ec1014489c8183e2751e56850
Joe Lohr <jlohr@autorisknow.com>
2023-04-08 22:56:43 -0500

    Update to game schedule list

--- resources/views/livewire/player/upcoming-games.blade.php
+++ resources/views/livewire/player/upcoming-games.blade.php
@@ -7,7 +7,6 @@
                     <tr>
                         <th scope="col" class="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">Date</th>
                         <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">Tournament</th>
-                        <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">Time</th>
                         <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">Location</th>
                         <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">Opponent</th>
                     </tr>
@@ -15,9 +14,11 @@
                     <tbody class="divide-y divide-gray-200 bg-white">
                     @foreach($games as $game)
                         <tr>
-                            <td class="whitespace-nowrap py-2 pl-0 pr-2 text-sm text-gray-500">{{ $game->date->format('F d, Y') }}</td>
+                            <td class="whitespace-nowrap py-2 pl-0 pr-2 text-sm text-gray-500">
+                                {{ $game->date->format('F d, Y') }}
+                                <span class="block">{{ $game->time->format('g:i A') }}</span>
+                            </td>
                             <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">{{ $game->tournament->name ?? '-' }}</td>
-                            <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">{{ $game->time->format('g:i A') }}</td>
                             <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
                                 @if($game->city)
                                     {{ $game->city }}, {{ $game->state }}
